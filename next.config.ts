6bba570af28d81e60650da41ec8dede6984a6dc2
minhzau <minhzau92@gmail.com>
2024-11-22 18:12:25 +0700

    first commit

---
+++ next.config.ts
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConFig} */
+const nextconfig = {
+  images: {
+    domains: ["lh3.googleusercontent.com", "images.unsplash.com", "plus.unsplash.com", "media.istockphoto.com"],
+  },
+};
+
+module.exports = nextconfig;
